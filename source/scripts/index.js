287b3b19a92197006cf044f07b78634533c35f9c
chepelova <168730690+chepelova@users.noreply.github.com>
2024-11-19 11:56:40 +0300

    исправляет js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,3 @@
 if (document.querySelector('.address__map-img--no-js')) {
-  document.querySelector('.address__map-img').classList.remove('.address__map-img--no-js')
+  document.querySelector('.address__map-img').classList.remove('address__map-img--no-js');
 }
